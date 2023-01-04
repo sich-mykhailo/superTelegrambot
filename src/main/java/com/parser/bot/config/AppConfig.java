d253e15c86e90c776bb2c12a9b2b63044e260954
sich-mykhailo <sichmisha@gmail.com>
2023-01-04 12:51:54 +0200

    cnange telegram bot version

--- src/main/java/com/parser/bot/config/AppConfig.java
+++ src/main/java/com/parser/bot/config/AppConfig.java
@@ -5,17 +5,28 @@ import com.parser.bot.service.Bot;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.LongPollingBot;
+import org.telegram.telegrambots.starter.SpringWebhookBot;
+import org.telegram.telegrambots.starter.TelegramBotInitializer;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-@Configuration
+import java.util.Collections;
+import java.util.List;
+
+
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Configuration
+@ConditionalOnProperty(prefix = "telegrambots", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class AppConfig {
     final BotConfig botConfig;
 
@@ -25,8 +36,19 @@ public class AppConfig {
     }
 
     @Bean
-    TelegramBotsApi telegramBotsApi() throws TelegramApiException {
+    @ConditionalOnMissingBean(TelegramBotsApi.class)
+    public TelegramBotsApi telegramBotsApi() throws TelegramApiException {
         return new TelegramBotsApi(DefaultBotSession.class);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public TelegramBotInitializer telegramBotInitializer(TelegramBotsApi telegramBotsApi,
+                                                         ObjectProvider<List<LongPollingBot>> longPollingBots,
+                                                         ObjectProvider<List<SpringWebhookBot>> webHookBots) {
+        return new TelegramBotInitializer(telegramBotsApi,
+                longPollingBots.getIfAvailable(Collections::emptyList),
+                webHookBots.getIfAvailable(Collections::emptyList));
     }
 
     @Bean
