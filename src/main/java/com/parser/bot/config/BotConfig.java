4e3d64dce2e9b417c9ef14afa9a462ac7d893ac7
sich-mykhailo <sichmisha@gmail.com>
2023-01-04 12:01:00 +0200

    added ngrok config

--- src/main/java/com/parser/bot/config/BotConfig.java
+++ src/main/java/com/parser/bot/config/BotConfig.java
@@ -8,50 +8,55 @@ import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
-import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Getter
 @Component
 @Slf4j
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class BotConfig {
+    @Value("${bot.server.ngrok}")
+    private String ngrokAddress;
     @Value("${bot.token}")
-    String botToken;
-
+    private String botToken;
     @Value("${bot.username}")
-    String botUserName;
-    @Value("${bot.webhook}")
-    String webHookPath;
+    private String botUserName;
 
     @PostConstruct
     public void setWebhook() {
         RestTemplate restTemplate = new RestTemplate();
+        String webHookPath = getWebHookPath();
         try {
             restTemplate.getForObject(Constants.TELEGRAM_URL
                     + "bot"
                     + botToken
                     + Constants.SET_WEBHOOK
                     + webHookPath, String.class);
-            log.info("updated WebHook");
+            log.info(String.format("updated WebHook to %s", webHookPath));
         } catch (Exception e) {
             log.warn("Can't set webhook");
         }
     }
 
+    private String getNgrokUrl() {
+        RestTemplate restTemplate = new RestTemplate();
+        List<LinkedHashMap<String, String>>
+                tunnels = (ArrayList) restTemplate.getForObject(ngrokAddress, HashMap.class).get("tunnels");
+        return tunnels.stream()
+                .findFirst().orElseThrow(()
+                        -> new RuntimeException("Can't get public url")).get("public_url");
+    }
+
     public void createMainMenu(Bot bot) {
         List<BotCommand> commandsList = new ArrayList<>();
-        commandsList.add(new BotCommand("/parse",  Emojis.BAR_CHART + " Відправити запит"));
+        commandsList.add(new BotCommand("/parse", Emojis.BAR_CHART + " Відправити запит"));
         commandsList.add(new BotCommand("/changemail", Emojis.EMAIL + " Змінити імейл"));
         commandsList.add(new BotCommand("/help", Emojis.SOS + " Допомога"));
         commandsList.add(new BotCommand("/checkrequests", Emojis.MAG + "Провірити кількість запитів"));
@@ -63,5 +68,9 @@ public class BotConfig {
         } catch (TelegramApiException e) {
             log.error("Can't create main menu", e);
         }
+    }
+
+    public String getWebHookPath() {
+        return getNgrokUrl();
     }
 }
