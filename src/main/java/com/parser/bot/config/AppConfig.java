a0d49e11e2871a79140063ffd27d1c5c8b41874b
sich-mykhailo <sichmisha@gmail.com>
2023-01-04 12:40:04 +0200

    cnange telegram bot version

--- src/main/java/com/parser/bot/config/AppConfig.java
+++ src/main/java/com/parser/bot/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.parser.bot.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.parser.bot.service.Bot;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -7,13 +8,26 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Configuration
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class AppConfig {
     final BotConfig botConfig;
+
+    @Bean
+    ObjectMapper customObjectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    TelegramBotsApi telegramBotsApi() throws TelegramApiException {
+        return new TelegramBotsApi(DefaultBotSession.class);
+    }
 
     @Bean
     public SetWebhook setWebhookInstance() {
